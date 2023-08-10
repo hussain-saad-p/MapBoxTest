a2320994e242e872d4d8e381fb0251c8e4cc0cad
hussain-saad <141621801+hussain-saad-p@users.noreply.github.com>
2023-08-10 12:12:54 +0500

    -Code Optimization and refactoring -Add marker to map -Show Marker with current location -Place Circle Shape on Map -Polyline shape done

--- app/src/main/java/com/example/mygroovyapplication/MainActivity.kt
+++ app/src/main/java/com/example/mygroovyapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mygroovyapplication
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.motion.widget.Debug.getLocation
@@ -18,10 +25,18 @@ import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
 import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PolygonAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createCircleAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.createPolygonAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.createPolylineAnnotationManager
 import com.mapbox.maps.plugin.locationcomponent.location
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
     companion object {
         private const val REQUEST_CODE = 123
@@ -35,8 +50,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initCode()
         setUpMapBox()
         askLocationPermissions()
+    }
+
+    private fun initCode() {
+        findViewById<View>(R.id.floatingActionButton).setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(view: View?) {
+        view?.let { v ->
+            when (v.id) {
+                R.id.floatingActionButton -> moveCameraToCurrentLocation()
+            }
+        }
     }
 
     private fun setUpMapBox() {
@@ -51,17 +79,17 @@ class MainActivity : AppCompatActivity() {
      */
     private fun askLocationPermissions() = if (ActivityCompat.checkSelfPermission(
             this,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION
+            Manifest.permission.ACCESS_COARSE_LOCATION
         ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
             this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION
         ) != PackageManager.PERMISSION_GRANTED
     ) {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
             ),
             REQUEST_CODE
         )
@@ -78,20 +106,20 @@ class MainActivity : AppCompatActivity() {
     private fun askBackgroundPermission() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         if (ActivityCompat.checkSelfPermission(
                 this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
                 this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
+                Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             if (ActivityCompat.checkSelfPermission(
                     this,
-                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 ActivityCompat.requestPermissions(
                     this,
-                    arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
                     REQUEST_CODE_BACKGROUND
                 );
             } else Toast.makeText(
@@ -120,12 +148,8 @@ class MainActivity : AppCompatActivity() {
             REQUEST_CODE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getLocation()
                 showCurrentLocationOnMap()
-            } else {
-                // Permission Denied
-                Toast.makeText(this, "your message", Toast.LENGTH_SHORT)
-                    .show()
-            }
-
+            } else Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT)
+                .show()     // Permission Denied
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
@@ -137,9 +161,37 @@ class MainActivity : AppCompatActivity() {
             mapView?.location?.updateSettings {// After the style is loaded, initialize the Location component
                 enabled = true
                 pulsingEnabled = true
+
+                moveCameraToCurrentLocation()
+                setLocationPuck()
+                addAnnotationToMap()
             }
         }
-        setLocationPuck()
+    }
+
+    private fun moveCameraToCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
+
+        LocationServices.getFusedLocationProviderClient(this).lastLocation.addOnSuccessListener {
+            mapView?.getMapboxMap()?.setCamera(
+                CameraOptions.Builder()
+                    .center(
+                        Point.fromLngLat(
+                            it.longitude, it.latitude
+                        )
+                    )
+                    .zoom(15.5)
+                    .bearing(-17.6)
+                    .build()
+            )
+        }
     }
 
     private fun setLocationPuck() {
@@ -169,70 +221,138 @@ class MainActivity : AppCompatActivity() {
                 }
             }.toJson()
         )
-
-        // set initial camera position
-//        val initialCameraOptions = CameraOptions.Builder()
-//            .center(Point.fromLngLat(-74.0066, 40.7135))
-//            .pitch(45.0)
-//            .zoom(15.5)
-//            .bearing(-17.6)
-//            .build()
-//
-//        val mapInitOptions = MapInitOptions(this, resourceOptions, mapOptions, plugins, initialCameraOptions, true)
-//        mapView = MapView(this, mapInitOptions)
-
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        LocationServices.getFusedLocationProviderClient(this).lastLocation.addOnSuccessListener {
-
-            mapView?.getMapboxMap()?.setCamera(
-                CameraOptions.Builder()
-                    .center(
-                        Point.fromLngLat(
-                            it.longitude, it.latitude
-                        )
-                    )
-                    .zoom(15.5)
-                    .bearing(-17.6)
-                    .build()
-            )
-        }
-
-        findViewById<View>(R.id.floatingActionButton).setOnClickListener {
-            LocationServices.getFusedLocationProviderClient(this).lastLocation.addOnSuccessListener {
-
-                mapView?.getMapboxMap()?.setCamera(
-                    CameraOptions.Builder()
-                        .center(
-                            Point.fromLngLat(
-                                it.longitude, it.latitude
-                            )
-                        )
-                        .zoom(15.5)
-                        .bearing(-17.6)
-                        .build()
-                )
-            }
-        }
-
     }
 
+    /**
+     * Add Annotations To Map
+     */
+    private fun addAnnotationToMap() {
+//        addMarkerAnnotationToMap()
+//        addCircleAnnotationToMap()
+//        addPolylineAnnotationToMap()
+        addPolygonAnnotationToMap()
+    }
+
+    /**
+     * Marker Annotation
+     */
+    private fun addMarkerAnnotationToMap() {
+        // Create an instance of the Annotation API and get the PointAnnotationManager.
+        bitmapFromDrawableRes(
+            this@MainActivity,
+            R.drawable.red_marker
+        )?.let {
+            val annotationApi = mapView?.annotations
+            val pointAnnotationManager = mapView?.let { it1 ->
+                annotationApi?.createPointAnnotationManager(it1)
+            }
+            // Set options for the resulting symbol layer.
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+                // Define a geographic coordinate.
+                .withPoint(Point.fromLngLat(18.06, 59.31))
+                // Specify the bitmap you assigned to the point annotation
+                // The bitmap will be added to map style automatically.
+                .withIconImage(it)
+
+            // Add the resulting pointAnnotation to the map.
+            pointAnnotationManager?.create(pointAnnotationOptions)
+        }
+    }
+
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) return null
+        return if (sourceDrawable is BitmapDrawable) sourceDrawable.bitmap
+        else {
+            // copying drawable object to not manipulate on the same reference
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
+    }
+
+    /**
+     * Circle Annotation
+     */
+    private fun addCircleAnnotationToMap() {
+        // Create an instance of the Annotation API and get the CircleAnnotationManager.
+        val annotationApi = mapView?.annotations
+        val circleAnnotationManager =
+            mapView?.let { annotationApi?.createCircleAnnotationManager(it) }
+        // Set options for the resulting circle layer.
+        val circleAnnotationOptions: CircleAnnotationOptions = CircleAnnotationOptions()
+            // Define a geographic coordinate.
+            .withPoint(Point.fromLngLat(14.06, 59.31))
+            // Style the circle that will be added to the map.
+            .withCircleRadius(8.0)
+            .withCircleColor("#ee4e8b")
+            .withCircleStrokeWidth(2.0)
+            .withCircleStrokeColor("#ffffff")
+        // Add the resulting circle to the map.
+        circleAnnotationManager?.create(circleAnnotationOptions)
+    }
+
+    /**
+     * Polyline Annotation
+     */
+    private fun addPolylineAnnotationToMap() {
+        // Create an instance of the Annotation API and get the polyline manager.
+        val annotationApi = mapView?.annotations
+        val polylineAnnotationManager = mapView?.let {
+            annotationApi?.createPolylineAnnotationManager(
+                it
+            )
+        }
+        // Define a list of geographic coordinates to be connected.
+        val points = listOf(
+            Point.fromLngLat(14.94, 79.25),
+            Point.fromLngLat(22.18, 79.37)
+        )
+        // Set options for the resulting line layer.
+        val polylineAnnotationOptions: PolylineAnnotationOptions = PolylineAnnotationOptions()
+            .withPoints(points)
+            // Style the line that will be added to the map.
+            .withLineColor("#ee4e8b")
+            .withLineWidth(5.0)
+        // Add the resulting line to the map.
+        polylineAnnotationManager?.create(polylineAnnotationOptions)
+    }
+
+    private fun addPolygonAnnotationToMap() {
+        // Create an instance of the Annotation API and get the polygon manager.
+        val annotationApi = mapView?.annotations
+        val polygonAnnotationManager = mapView?.let {
+            annotationApi?.createPolygonAnnotationManager(
+                it
+            )
+        }
+        // Define a list of geographic coordinates to be connected.
+        val points = listOf(
+            listOf(
+                Point.fromLngLat(17.94, 59.25),
+                Point.fromLngLat(18.18, 59.25),
+                Point.fromLngLat(18.18, 59.37),
+                Point.fromLngLat(17.94, 59.37)
+            )
+        )
+        // Set options for the resulting fill layer.
+        val polygonAnnotationOptions: PolygonAnnotationOptions = PolygonAnnotationOptions()
+            .withPoints(points)
+            // Style the polygon that will be added to the map.
+            .withFillColor("#ee4e8b")
+            .withFillOpacity(0.4)
+        // Add the resulting polygon to the map.
+        polygonAnnotationManager?.create(polygonAnnotationOptions)
+    }
 
     /**
      * MapBox Lifecycle Methods
